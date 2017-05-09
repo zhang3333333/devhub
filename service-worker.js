588b53158cee8719567eb2eac0c12bf193807b31
Bruno Lemos <brunohplemos@gmail.com>
2017-05-09 20:45:20 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["favicon.ico","819bb2eabbb5f18b81c2182bed5fdb92"],["index.html","efb4c71661e6283e342827eb559aae4e"],["manifest.json","bee92cb425f021be5caa712828792c44"],["report.html","3802beb27d1a439f59b7cbbca90f6b36"],["static/css/error.css","57a430b84df5b5e0601a65afaa25ac19"],["static/css/main.93ff9849.css","6250cc85bcdc476a6e9a8cd7114bcb1b"],["static/media/Octicons.145eaf50.ttf","145eaf5090710407a72d93f9962577ef"],["static/media/logo.png","3bf103bb0418372a74c132637514f138"],["static/media/logo@2x.png","63d2dd38214d58e70666048a1e16ff5d"],["static/media/logo@3x.png","446e75c8f140e6d2beb1871683da77f1"]];
+var precacheConfig = [["favicon.ico","819bb2eabbb5f18b81c2182bed5fdb92"],["index.html","c56a60b8e0e38a1691da1bd3bf043ee4"],["manifest.json","bee92cb425f021be5caa712828792c44"],["report.html","82d41715eaa8366f97ab854842e89c9f"],["static/css/error.css","57a430b84df5b5e0601a65afaa25ac19"],["static/css/main.8d100994.css","25b76d02890419970d1b20d3d74a98e7"],["static/media/Octicons.145eaf50.ttf","145eaf5090710407a72d93f9962577ef"],["static/media/logo.png","3bf103bb0418372a74c132637514f138"],["static/media/logo@2x.png","63d2dd38214d58e70666048a1e16ff5d"],["static/media/logo@3x.png","446e75c8f140e6d2beb1871683da77f1"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
