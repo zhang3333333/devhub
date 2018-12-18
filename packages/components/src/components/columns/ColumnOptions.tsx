a3cd8b51152b6a025e1a6112cdac75ba1d5b854d
Bruno Lemos <brunohplemos@gmail.com>
2018-12-17 23:18:44 -0200

    [Android] Fix blank screen after removing last column

--- packages/components/src/components/columns/ColumnOptions.tsx
+++ packages/components/src/components/columns/ColumnOptions.tsx
@@ -563,7 +563,7 @@ export function ColumnOptions(props: ColumnOptionsProps) {
           <ColumnHeaderItem
             analyticsLabel="remove_column"
             iconName="trashcan"
-            onPress={() => deleteColumn(column.id)}
+            onPress={() => deleteColumn({ columnId: column.id, columnIndex })}
             text="Remove"
           />
         </View>
